ae8064094a9d447ac023071a56c9302f80040c10
pratikmahulkar <34033140+pratikmahulkar@users.noreply.github.com>
2017-11-28 21:11:00 +0530

    v9

--- index.js
+++ index.js
@@ -4,19 +4,8 @@ var csvtojson = require('csvtojson');
 var http = require('https');
 var fs = require('fs');
 var unzip = require("unzip");
-var test = false;
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
-//app.get('/', function (request, response) {
-//    response.send('Hello World!');
-//});
-//app.get('/index.html', function (request, response) {
-//    response.sendFile('index.html');
-//});
-//app.get('/home.html', function (request, response) {
-//    response.sendFile('home.html');
-//});
 
 var prepareURL = function (selectedDate) {
     var formattedDate = "";
@@ -35,17 +24,21 @@ var getFormattedDate = function (selectedDate) {
     return formattedDate;
 };
 
-app.get('/api/:date', function 
-(req, res) {
+app.get('/api/:date', function (req, res) {
     var date = req.params['date'];
     console.log(req.params);
     var url = prepareURL(new Date(date));
     var header = null;
     var rows = [];
     var formattedDate = getFormattedDate(new Date(date));
-
-    if (test) {
-        fs.createReadStream(formattedDate + '.zip')
+    var file = fs.createWriteStream(formattedDate + ".zip");
+    var request = http.request(url);
+    request.setHeader('Content-Type', 'text/zip');
+    request.setHeader('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0');
+    request.end();
+    request.once('response', function (response) {
+        response.pipe(file).on('close', function () {
+            fs.createReadStream(formattedDate + '.zip')
             .pipe(unzip.Parse())
             .on('entry', function (entry) {
                 var fileName = entry.path;
@@ -64,38 +57,9 @@ app.get('/api/:date', function
                     res.json(rows);
                     console.log('end');
                 });
-            });
-    } else {
-        var file = fs.createWriteStream(formattedDate + ".zip");
-        var request = http.request(url);
-        request.setHeader('Content-Type', 'text/zip');
-        request.setHeader('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0');
-        request.end();
-        request.once('response', function (response) {
-            response.pipe(file).on('close', function () {
-                fs.createReadStream(formattedDate + '.zip')
-            .pipe(unzip.Parse())
-            .on('entry', function (entry) {
-                var fileName = entry.path;
-                var type = entry.type; // 'Directory' or 'File'
-                var size = entry.size;
-                entry.pipe(fs.createWriteStream(formattedDate + '.csv'));
-                entry.autodrain();
-            })
-            .on('close', function () {
-                csvtojson()
-                .fromFile(formattedDate + ".csv")
-                .on('json', function (data) {
-                    rows.push(data);
-                })
-                .on('done', function () {
-                    res.json(rows);
-                    console.log('end');
-                });
-            });
             });
         });
-    }
+    });
 
 });
 app.listen(app.get('port'), function () {
